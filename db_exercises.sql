9db9fb5cb50ba551a4e782c1a14f404fb6364343
Steve Kane <quantumkane00@gmail.com>
2020-12-16 11:19:46 -0600

    intro database exercise

---
+++ db_exercises.sql
@@ -0,0 +1,15 @@
+
+# listing all my available databases
+SHOW DATABASES;
+
+# switching to a specific db
+use elo_db;
+
+# identifying current db
+select database();
+
+# switching to a different DB
+use sakila;
+
+# identifying current db
+select database();
