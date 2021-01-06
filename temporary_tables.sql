3d3dc135e75cb0052de3a729a7e277365117b025
Steve Kane <quantumkane00@gmail.com>
2021-01-06 10:01:02 -0600

    setting up

---
+++ temporary_tables.sql
@@ -0,0 +1,21 @@
+
+# 1. Using the example from the lesson, re-create the employees_with_departments table.
+
+# a) Add a column named full_name to this table. It should be a VARCHAR whose length is the sum of the lengths of the first name and last name columns
+
+
+# b) Update the table so that full name column contains the correct data
+
+
+# c) Remove the first_name and last_name columns from the table.
+
+
+# d) What is another way you could have ended up with this same table?
+
+
+# 2. Create a temporary table based on the payment table from the sakila database.
+     Write the SQL necessary to transform the amount column such that it is stored as an      integer representing the number of cents of the payment. 
+     
+     
+# 3. Find out how the current average pay in each department compares to the overall, historical average pay. In order to make the comparison easier, you should use the Z-score for salaries. In terms of salary, what is the best department right now to work for? The worst?
+
