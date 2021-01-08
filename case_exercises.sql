a40e76b2fdb5460ff725780f1832ba0cb3d5bf94
Steve Kane <quantumkane00@gmail.com>
2021-01-08 10:44:17 -0600

    amended

--- case_exercises.sql
+++ case_exercises.sql
@@ -8,6 +8,7 @@ SELECT
     dept_no,
     to_date,
     from_date,
+    hire_date,
     IF(to_date = '9999-01-01', 1, 0) AS is_current_employee
 FROM dept_emp
 JOIN (SELECT 
@@ -16,7 +17,8 @@ JOIN (SELECT
     FROM dept_emp
     GROUP BY emp_no) AS last_dept
 ON dept_emp.emp_no = last_dept.emp_no
-    AND dept_emp.to_date = last_dept.max_date;
+    AND dept_emp.to_date = last_dept.max_date
+JOIN employees ON employees>dept_no = dempt_emp.emp_no;
 
 
 # 2. Write a query that returns all employee names (previous and current), and a new column 'alpha_group' that returns 'A-H', 'I-Q', or 'R-Z' depending on the first letter of their last name.
@@ -25,8 +27,7 @@ SELECT first_name, last_name,
 	CASE 
 		WHEN LEFT(last_name, 1) IN ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H') THEN 'A-H'
 		WHEN LEFT(last_name, 1) IN ('I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q') THEN 'I-Q'
-		WHEN LEFT(last_name, 1) IN ('R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z') THEN 'R-Z'
-		ELSE last_name
+		ELSE 'R-Z'
 		END AS alpha_group
 FROM employees;	
 
@@ -35,12 +36,29 @@ FROM employees;
 
 SELECT birth_date,
 	CASE 
-	WHEN birth_date BETWEEN '1950-01-01' AND '1959-12-31' THEN 'Born in the 50s'
-	WHEN birth_date BETWEEN '1960-01-01' AND '1969-12-31' THEN 'Born in the 60s'
-	ELSE birth_date
-	END AS by_decade
+        WHEN birth_date BETWEEN '1950-01-01' AND '1959-12-31' THEN 'Born in the 50s'
+        WHEN birth_date BETWEEN '1960-01-01' AND '1969-12-31' THEN 'Born in the 60s'
+        ELSE birth_date
+        END AS by_decade
+        COUNT(*) AS total
 	FROM employees
-ORDER BY birth_date DESC;
+GROUP BY by_decade;
 
 
 # Bonus: What is the current average salary for each of the following department groups: R&D, Sales & Marketing, Prod & QM, Finance & HR, Customer Service?
+
+SELECT
+        CASE 
+            WHEN dept_name IN ('research', 'development') THEN 'R&D'
+            WHEN dept_name IN ('sales', 'marketing') THEN 'Sales & Marketing' 
+            WHEN dept_name IN ('Production', 'Quality Management') THEN 'Prod & QM'
+            WHEN dept_name IN ('Finance', 'Human Resources') THEN "Finance & HR"
+            ELSE dept_name
+            END AS dept_group,
+        ROUND(AVG(salary)) AS avg_salary
+FROM salaries 
+JOIN dept_emp USING(emp_no)
+JOIN departments USING(dept_no)
+WHERE salaries.to_date > curdate() 
+	AND dept_emp.to_date > curdate()
+GROUP BY dept_group;
